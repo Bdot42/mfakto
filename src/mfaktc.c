3dda514faeee8484d651835337c6c91f4648cb9d
Bertram Franz <bertramf@gmx.net>
2012-04-10 17:00:59 +0200

    re-enable all self-tests, put the new kernel in the right selection order

--- src/mfaktc.c
+++ src/mfaktc.c
@@ -150,10 +150,11 @@ other return value
   }
 
   if(use_kernel == AUTOSELECT_KERNEL)
-  {
+  {  // this is probably the speed order for Cayman (HD6970)
     if (mystuff->preferredKernel == _71BIT_MUL24)  // maybe this can be bound to some version/feature/capability or be tested during selftest
     {
       if      ((bit_min >= 61) && (bit_max <= 72))                              use_kernel = _71BIT_MUL24;
+      else if ((bit_min >= 60) && (bit_max <= 73) && (bit_max - bit_min == 1))  use_kernel = BARRETT73_MUL15;
       else if ((bit_min >= 63) && (bit_max <= 70) && (bit_max - bit_min == 1))  use_kernel = BARRETT72_MUL24;
       else if                     (bit_max <= 64)                               use_kernel = _63BIT_MUL24;
       else if ((bit_min >= 64) && (bit_max <= 79))                              use_kernel = BARRETT79_MUL32;
@@ -161,12 +162,11 @@ other return value
 //      else if                     (bit_max <  95)                               use_kernel = _95BIT_64_OpenCL;
     }
     else
-    {
-      if      ((bit_min >= 60) && (bit_max <= 71) && (bit_max - bit_min == 1))  use_kernel = BARRETT73_MUL15;
-      else if ((bit_min >= 63) && (bit_max <= 70) && (bit_max - bit_min == 1))  use_kernel = BARRETT72_MUL24;
-      else if ((bit_min >= 64) && (bit_max <= 79))                              use_kernel = BARRETT79_MUL32;
-//      if      ((bit_min >= 64) && (bit_max <= 79))                              use_kernel = BARRETT79_MUL32;
+    {  // this is the speed order for HD5770, for instance
+      if      ((bit_min >= 63) && (bit_max <= 70) && (bit_max - bit_min == 1))  use_kernel = BARRETT72_MUL24;
+      else if ((bit_min >= 60) && (bit_max <= 73) && (bit_max - bit_min == 1))  use_kernel = BARRETT73_MUL15;
       else if ((bit_min >= 61) && (bit_max <= 72))                              use_kernel = _71BIT_MUL24;
+      else if ((bit_min >= 64) && (bit_max <= 79))                              use_kernel = BARRETT79_MUL32;
       else if                     (bit_max <= 64)                               use_kernel = _63BIT_MUL24;
       else if ((bit_min >= 64) && (bit_max <= 92) && (bit_max - bit_min == 1))  use_kernel = BARRETT92_MUL32;
 //      else if                     (bit_max <  95)                               use_kernel = _95BIT_64_OpenCL;
@@ -541,11 +541,11 @@ RET_ERROR we might have a serios problem
 
 /* create a list which kernels can handle this testcase */
     j = 0;
-//    if (bit_min[ind] <= 63)                            kernels[j++] = _63BIT_MUL24;
+    if (bit_min[ind] <= 63)                            kernels[j++] = _63BIT_MUL24;
     if ((bit_min[ind] >= 61) && (bit_min[ind] < 72))   kernels[j++] = _71BIT_MUL24;
     if ((bit_min[ind] >= 64) && (bit_min[ind] < 79))   kernels[j++] = BARRETT79_MUL32; 
-//    if ((bit_min[ind] >= 64) && (bit_min[ind] < 92))   kernels[j++] = BARRETT92_MUL32; /* no need to check bit_max - bit_min == 1 ;) */
-    if ((bit_min[ind] >= 63) && (bit_min[ind] < 71))   kernels[j++] = BARRETT72_MUL24;
+    if ((bit_min[ind] >= 64) && (bit_min[ind] < 92))   kernels[j++] = BARRETT92_MUL32; /* no need to check bit_max - bit_min == 1 ;) */
+    if ((bit_min[ind] >= 63) && (bit_min[ind] < 70))   kernels[j++] = BARRETT72_MUL24;
     if ((bit_min[ind] >= 60) && (bit_min[ind] < 73))   kernels[j++] = BARRETT73_MUL15;
 //
 //      if ((bit_min[ind] >= 64) && (bit_min[ind]) < 79)   kernels[j++] = _95BIT_64_OpenCL; // currently just a test for no sieving at all
