4c622600bea668bd100d9cc6557218ebc772183c
Bertram Franz <bertramf@gmx.net>
2012-04-11 16:02:33 +0200

    reverting most changes in barrett15.cl

--- src/params.h
+++ src/params.h
@@ -38,7 +38,7 @@ than an equal SIEVE_SIZE_LIMIT #define.
 
 */
 
-#define SIEVE_SIZE_LIMIT 64
+#define SIEVE_SIZE_LIMIT 36
 
 
 /******************
@@ -83,7 +83,7 @@ code path - not used by mfakto, see kernel trace */
    pure times needed to copy the data over, and to test the FC's of the chunk
    (pure run time per kernel invokation) - best measure to compare changes in
    the kernels, or drivers. */
-#define CL_PERFORMANCE_INFO
+//#define CL_PERFORMANCE_INFO
 
 
 /* Tell the OpenCL compiler to create debuggable code for the Kernels */
