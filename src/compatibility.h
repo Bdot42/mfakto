274417112c0c979f05832d59eaafc6f9a3873a06
k3ack3r <pcexplorer12@gmail.com>
2014-01-14 08:26:02 -0800

    revert changes.. need to think

--- src/compatibility.h
+++ src/compatibility.h
@@ -29,16 +29,7 @@ along with mfaktc (mfakto).  If not, see <http://www.gnu.org/licenses/>.
   #define PRIx64 "llx"
   
   #define strncasecmp _strnicmp
-#endif
-
-#ifdef __MINGW32__
-  #include <inttypes.h>
-  /*
-   #define PRId64 "I64d"
-   #define PRIu64 "I64u"
-   #define PRIx64 "I64x"
-  */
-#else 
+#else
   #define PRId64 "Ld"
   #define PRIu64 "Lu"
   #define PRIx64 "Lx"
